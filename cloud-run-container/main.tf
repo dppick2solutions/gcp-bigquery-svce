03a66efac653449a8b0beffec17957f0ce633855
DeVante Pickering <devante@pick2solutions.com>
2025-04-23 18:56:08 -0400

    add iam role

--- cloud-run-container/main.tf
+++ cloud-run-container/main.tf
@@ -13,3 +13,13 @@ resource "google_artifact_registry_repository" "repo" {
     immutable_tags = true
   }
 }
+
+resource "google_artifact_registry_repository_iam_binding" "binding" {
+  project = google_artifact_registry_repository.repo.project
+  location = google_artifact_registry_repository.repo.location
+  repository = google_artifact_registry_repository.repo.name
+  role = "roles/artifactregistry.repoAdmin"
+  members = [
+    "bigquery-pipeline-demo@pick2-bigquery-demo.iam.gserviceaccount.com", #parameter later.
+  ]
+}
