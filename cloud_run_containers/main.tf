676ed1c75cc612a36be1de5670dec55c1e207c17
DeVante Pickering <devante@pick2solutions.com>
2025-04-23 19:46:30 -0400

    update image names

--- cloud_run_containers/main.tf
+++ cloud_run_containers/main.tf
@@ -10,7 +10,7 @@ resource "google_artifact_registry_repository" "repo" {
   project       = data.google_project.project.project_id
 
   docker_config {
-    immutable_tags = true
+    immutable_tags = false
   }
 }
 
