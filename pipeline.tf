82b5be4912d35146f0405d34cc6b136f9c3ed822
DeVante Pickering <devante@pick2solutions.com>
2025-04-23 19:21:20 -0400

    refactor to a cloudrun approach

--- pipeline.tf
+++ pipeline.tf
@@ -1,25 +1,3 @@
-data "archive_file" "export_to_gcs" {
-  type        = "zip"
-  output_path = "export-to-gcs.zip"
-  source_dir  = "functions/export_to_gcs/"
-}
-resource "google_storage_bucket_object" "export_to_gcs_zip" {
-  name   = "export_to_gcs.zip"
-  bucket = google_storage_bucket.rawfiles.name
-  source = data.archive_file.export_to_gcs.output_path
-}
-
-data "archive_file" "gcs_to_bigquery" {
-  type        = "zip"
-  output_path = "gcs-to-bigquery.zip"
-  source_dir  = "functions/gcs_to_bigquery/"
-}
-resource "google_storage_bucket_object" "gcs_to_bigquery_zip" {
-  name   = "gcs_to_bigquery.zip"
-  bucket = google_storage_bucket.rawfiles.name
-  source = data.archive_file.gcs_to_bigquery.output_path
-}
-
 ## TODO: Enable Cloud Functions API
 ## TODO: Enable Cloud Build API
 ## TODO: Enable EventArc API
@@ -57,38 +35,38 @@ resource "google_storage_bucket_object" "gcs_to_bigquery_zip" {
 #   }
 #   depends_on = [google_storage_bucket_object.export_to_gcs_zip]
 # }
-resource "google_cloudfunctions2_function" "gcs_to_bigquery" {
-  name        = "export-to-bigquery"
-  location    = "us-central1"
-  description = "Loads CSV from GCS into a BigQuery table"
+# resource "google_cloudfunctions2_function" "gcs_to_bigquery" {
+#   name        = "export-to-bigquery"
+#   location    = "us-central1"
+#   description = "Loads CSV from GCS into a BigQuery table"
 
-  build_config {
-    runtime     = "python310"
-    entry_point = "gcs_to_bigquery"
-    source {
+#   build_config {
+#     runtime     = "python310"
+#     entry_point = "gcs_to_bigquery"
+#     source {
 
-      storage_source {
-        bucket = google_storage_bucket.rawfiles.name
-        object = google_storage_bucket_object.gcs_to_bigquery_zip.name
-      }
-    }
-  }
+#       storage_source {
+#         bucket = google_storage_bucket.rawfiles.name
+#         object = google_storage_bucket_object.gcs_to_bigquery_zip.name
+#       }
+#     }
+#   }
 
-  service_config {
-    max_instance_count = 1
-    available_memory   = "256M"
-    timeout_seconds    = 60
-  }
-  event_trigger {
-    event_type = "google.cloud.storage.object.v1.finalized"
-    retry_policy = "RETRY_POLICY_DO_NOT_RETRY"
-    event_filters {
-      attribute = "bucket"
-      value     = google_storage_bucket.rawfiles.name
-    }
-  }
-  depends_on = [google_storage_bucket_iam_member.eventarc_bucket_permissions, google_storage_bucket_object.gcs_to_bigquery_zip]
-}
+#   service_config {
+#     max_instance_count = 1
+#     available_memory   = "256M"
+#     timeout_seconds    = 60
+#   }
+#   event_trigger {
+#     event_type = "google.cloud.storage.object.v1.finalized"
+#     retry_policy = "RETRY_POLICY_DO_NOT_RETRY"
+#     event_filters {
+#       attribute = "bucket"
+#       value     = google_storage_bucket.rawfiles.name
+#     }
+#   }
+#   depends_on = [google_storage_bucket_iam_member.eventarc_bucket_permissions, google_storage_bucket_object.gcs_to_bigquery_zip]
+# }
 
 # Define the storage bucket and the service account for Eventarc
 resource "google_storage_bucket_iam_member" "eventarc_bucket_permissions" {
